a208b4f06d08b12b1eb1456f00446b1b10417220
Wenfang Du <no_reply_mail@163.com>
2024-03-03 16:58:14 +0800

    feat: init

---
+++ main.js
@@ -0,0 +1,26 @@
+const timer = document.querySelector('#timer')
+let seconds = 25 * 60
+let running = true
+
+const convert = seconds => {
+  if (!seconds) {
+    clearInterval(interval)
+    return '00:00'
+  }
+  const minutes = String(Math.floor(seconds / 60)).padStart(2, 0)
+  seconds = String(seconds % 60).padStart(2, 0)
+  return `${minutes}:${seconds}`
+}
+
+const tick = () => {
+  timer.innerHTML = document.title = convert(seconds)
+  seconds = Math.max(seconds - 1, 0)
+}
+
+tick()
+
+const interval = setInterval(() => running && tick(), 1000)
+
+timer.onclick = () => (running = !running)
+
+onkeyup = ({ key }) => key === ' ' && timer.onclick()
