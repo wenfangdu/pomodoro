468a58c6e34c8a69d71ef00741a09c6ac33aaefb
Wenfang Du <no_reply_mail@163.com>
2024-03-10 20:54:13 +0800

    feat: cache roboto mono

--- workbox-config.cjs
+++ workbox-config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   globDirectory: 'dist/',
-  globPatterns: ['**/*.{js,css,png,html}'],
+  globPatterns: ['**/*.{js,css,png,html,woff2}'],
   swDest: 'dist/sw.js',
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
 }
